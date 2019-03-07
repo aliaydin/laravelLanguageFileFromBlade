37fafbf40ae0d8c7ffc62a21bf3896b0ed12c29e
Ali Aydın <aliaydin06@gmail.com>
2019-03-07 15:27:41 +0300

    tekrarlı alanlar engellendi

--- langenerator.php
+++ langenerator.php
@@ -8,15 +8,15 @@ $content = file_get_contents($path . $file);
 preg_match_all('/({{ __\(\')(.*)(\.)(.*)(\')(.*)(\) }})/', $content, $matches);
 
 $arrays = [];
-foreach ($matches[2] as $key => $val) {
-    $arrays[$val][] = "'{$matches[4][$key]}' => '{$matches[4][$key]}',";
+foreach ($matches[2] as $index => $result) {
+    $arrays[$result][$matches[4][$index]] = $matches[4][$index];
 }
 
 foreach ($arrays as $group => $group_val) {
     echo "<h3>$group</h3>";
     echo 'return [<br>';
-    foreach ($group_val as $item) {
-        echo $item . '<br>';
+    foreach ($group_val as $key => $value) {
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;'$key' => '$value'," . '<br>';
     }
     echo '];<br><br>';
 }
